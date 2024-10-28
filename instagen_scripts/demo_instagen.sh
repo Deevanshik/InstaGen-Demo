4b0d129cdacd0fe3e7fa013c382aec9f2304d068
Deevanshik <137674395+Deevanshik@users.noreply.github.com>
2024-10-28 20:32:29 +0530

    Changed the image name
    Changed the name of the image which stores the final results

--- instagen_scripts/demo_instagen.sh
+++ instagen_scripts/demo_instagen.sh
@@ -5,4 +5,4 @@ python demo.py \
     --detector_ckpt checkpoints/instagen-4scale_fd_8xb2-12e_coco.pth \
     --cls_names 'cycle, hat' \
     --score_thr 0.4 \
-    --out demo/instagen_pred.jpg
+    --out demo/deevanshik.jpg
